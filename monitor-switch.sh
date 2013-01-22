7e09dd26b160e4ac386da7c1ad9d8de0687cf7fc
Pavel Sedlák <psedlak@redhat.com>
2013-01-22 23:43:35 +0100

    monitor-switch.sh: easier/bindable monitor layout switching

---
+++ monitor-switch.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# use configuration given as argument
+CONFIG=$1
+
+if ! which xrandr &> /dev/null; then
+    echo "No xrandr command available ... you have to fix it."
+    exit 1
+fi
+
+# if specified, otherwise try autodetection
+if [[ -z "$CONFIG" ]]; then
+    if xrandr -q | grep "HDMI1 connected"; then
+        CONFIG=work
+    elif [[ $(xrandr -q | grep " connected "|wc -l) == 1 ]]; then
+        CONFIG=default
+    fi
+    # place your autodetection here
+
+    #echo "Autodetected monitor layout: $CONFIG"
+fi
+
+case $CONFIG in
+    work)
+        xrandr --output LVDS1 --auto --right-of HDMI1 --output HDMI1 --auto --primary
+        ;;
+    default)
+        # use just display of nb
+        xrandr --output LVDS1 --auto --rotate normal --reflect normal --primary
+        ;;
+    -h|--help|help|*)
+        echo "Usage:"
+        echo "$0 [work | default]" # list possible configurations here
+        ;;
+esac
+
