5569679e013697a6d9c2999c7e1174f1162761f9
Queria Sa-Tas <public@sa-tas.net>
2013-02-17 15:34:08 +0100

    added colors-test.sh - print args with different colors

---
+++ colors-test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+MSG="$*"
+[[ -z "$MSG" ]] && MSG="test message"
+        #echo -e "$C$CTP-$CLR 〒review/afazekas/unify-wait\033[00m";
+for CLR in $(seq 40); do
+    for CTP in 0 1; do
+        C="\033[0${CTP};${CLR}m";
+        echo -e "$C$CTP-$CLR $MSG\033[00m";
+    done
+done
