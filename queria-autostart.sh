c722899634856fd23db4611548eb5d865eaebf20
Queria Sa-Tas <public@sa-tas.net>
2013-10-30 15:55:03 +0100

    queria-autostart: tweak touchpad sensitivity

--- queria-autostart.sh
+++ queria-autostart.sh
@@ -14,5 +14,9 @@ if [[ ! -z "$TPNAME" ]]; then
     xinput set-prop "$TPNAME" 'Evdev Wheel Emulation Button' 2
     xinput set-prop "$TPNAME" 'Evdev Wheel Emulation' 1
     xinput set-prop "$TPNAME" 'Evdev Wheel Emulation Axes' 6 7 4 5
+    xinput set-prop "$TPNAME" 'Device Accel Profile' 3
+    xinput set-prop "$TPNAME" 'Device Accel Velocity Scaling' 50
 fi
+
+qsrun &
 
