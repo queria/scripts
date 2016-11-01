42c45d064c6dcaf90513c1cb60a5c6db5978d78e
Pavel Sedlák <psedlak@redhat.com>
2016-11-01 17:51:50 +0100

    colors-test: print copy-pasteable examples

--- colors-test.sh
+++ colors-test.sh
@@ -4,9 +4,9 @@ MSG="$*"
 [[ -z "$MSG" ]] && read -t 10 -p "Type message or press enter" MSG
 [[ -z "$MSG" ]] && MSG="test message"
         #echo -e "$C$CTP-$CLR 〒review/afazekas/unify-wait\033[00m";
-for CLR in $(seq 40); do
+for CLR in $(seq 36); do
     for CTP in 0 1; do
         C="\033[0${CTP};${CLR}m";
-        echo -e "$C$CTP-$CLR $MSG\033[00m";
+        echo -e "${C}\\\033[0${CTP};${CLR}m ${MSG}\033[00m";
     done
 done
