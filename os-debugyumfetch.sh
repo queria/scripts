80491f8f758defab6e70cc1779103a92e2664784
Pavel Sedlák <psedlak@redhat.com>
2017-05-09 12:31:07 +0200

    os-debugyumfetch - loop yum install to debug rpm download flakiness
    as report says it's not reproducible with wget/...,
    lets grab yum's urlgrabber view of the situation :]

---
+++ os-debugyumfetch.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -ex
+
+MAXN=3
+
+LOGS='yum.log yum_err.log urlgrab.log'
+FAILDIR="failed_$(date "+%y%m%d_%H_%M_%S")"
+
+export URLGRABBER_DEBUG="1,urlgrab.log"
+
+N=0
+while [[ $N -lt $MAXN ]]; do
+    rm -f $LOGS
+
+    yum -d 10 --rpmverbosity=10 -y reinstall ipmitool 2>yum_err.log | tee yum.log || true
+    if [[ "${PIPESTATUS[0]}" != "0" ]]; then
+        mkdir -p $FAILDIR/$N;
+        mv $LOGS $FAILDIR/$N/;
+    fi
+
+    N=$(( $N + 1 ))
+
+    sleep 1
+done
